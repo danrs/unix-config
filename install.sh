4b7a5cafcc65e068149f9aee800ec323a4e609db
Daniel Smith <danrs@users.noreply.github.com>
2021-06-11 21:20:34 +0800

    zsh rearrangement wip

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # This repo holds all my unix config files
-echo "Symlinking unix-config to home directory:"
-ln -s ~/unix-config/.[!.]* ~
-rm ~/.git
-rm ~/.gitignore
+echo "Symlinking configs to home directory:"
+ln -s ~/unix-config/git/.[!.]* ~
+ln -s ~/unix-config/vim/.[!.]* ~
+ln -s ~/unix-config/zsh/.[!.]* ~
+ln -s ~/unix-config/bash/.[!.]* ~
+#rm ~/.git
+#rm ~/.gitignore
+chsh -s $(which zsh)
+zsh zsh/.zim/zimfw.zsh install
 
-echo "installing git-aware-prompt"
-mkdir -p  ~/.bash
-git clone git://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
+#echo "installing git-aware-prompt"
+#mkdir -p  ~/.bash
+#git clone git://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
 
 echo "To install vim plugins, open vim and run"
 echo ":PlugInstall"
 
-# make ctrl+tab change tab in ubuntu terminal
+# make ctrl+tab change tab in gnome terminal
 which gsettings && \
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary>Tab' && \
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Tab'
