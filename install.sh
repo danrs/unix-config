c32c1337c310c877fb543886a6414912075a3e67
Daniel Smith <danrs@users.noreply.github.com>
2021-06-13 11:37:53 +0800

    Cleaned up install process

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "Installing tools"
+sudo apt install vim git zsh
+
 # This repo holds all my unix config files
 echo "Symlinking configs to home directory:"
+rm ~/.bashrc
 ln -s ~/unix-config/git/.[!.]* ~
 ln -s ~/unix-config/vim/.[!.]* ~
 ln -s ~/unix-config/zsh/.[!.]* ~
 ln -s ~/unix-config/bash/.[!.]* ~
-#rm ~/.git
-#rm ~/.gitignore
+
+echo "Setting up zsh as default shell"
 chsh -s $(which zsh)
 zsh zsh/.zim/zimfw.zsh install
 
@@ -19,6 +23,7 @@ echo "To install vim plugins, open vim and run"
 echo ":PlugInstall"
 
 # make ctrl+tab change tab in gnome terminal
+echo "Checking for gnome settings to update terminal controls:"
 which gsettings && \
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary>Tab' && \
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Tab'
