35aaa9627a7d4ea1a8076559f9dc1a2005fe6c41
Daniel Smith <danrs@users.noreply.github.com>
2021-06-14 11:43:13 +0800

    added force option

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "Installing tools"
-sudo apt install vim git zsh
+sudo apt install -y vim git zsh
 
 # This repo holds all my unix config files
+if [ $1 = "-f" ]
+then
+    FORCE_LINK="-f"
+    echo FORCE LINKING
+fi
 echo "Symlinking configs to home directory:"
-rm ~/.bashrc
-ln -s ~/unix-config/git/.[!.]* ~
-ln -s ~/unix-config/vim/.[!.]* ~
-ln -s ~/unix-config/zsh/.[!.]* ~
-ln -s ~/unix-config/bash/.[!.]* ~
+ln -s $FORCE_LINK ~/unix-config/git/.[!.]* ~
+ln -s $FORCE_LINK ~/unix-config/vim/.[!.]* ~
+ln -s $FORCE_LINK ~/unix-config/zsh/.[!.]* ~
+ln -s $FORCE_LINK ~/unix-config/bash/.[!.]* ~
 
 echo "Setting up zsh as default shell"
 chsh -s $(which zsh)
 zsh zsh/.zim/zimfw.zsh install
-
-#echo "installing git-aware-prompt"
-#mkdir -p  ~/.bash
-#git clone git://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
 
 echo "To install vim plugins, open vim and run"
 echo ":PlugInstall"
